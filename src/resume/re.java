810c91cc3168af014928ea851c4623dffc838440
Shankar.jogi <Shankar.jogi@wslkcmp2f-49.SLKSOFT.COM>
2019-07-31 10:57:18 +0530

    first

---
+++ src/resume/re.java
@@ -0,0 +1,88 @@
+package resume;
+
+public class re {
+
+}
+OBJECTIVE 
+
+
+To get a challenging position in a growing company where I can utilize my skills and 
+knowledge. I’m good learner, like R&D for new technologies and want to be a part of good organization. 
+ 
+  
+TECHNICALPROFILE 
+ 
+ 
+Operating Systems 	Microsoft Windows 
+Servers 	Tomcat 
+Languages 	Java, C ,
+RDBMS 	MySQL 
+Web & Distributed Web Technologies 	Servlet, JSP 
+Framework 	Spring MVC 
+ORM Tool 	JDBC 
+IDE’s 	Eclipse 
+Web Development 	HTML,CSS, JSP 
+Mainframe Technology	JCL,COBOL,DB2,VSAM and basics in CICS
+ 
+ 
+ 
+ 
+EDUCATION QUALIFICATION 
+ 
+Bachelor of Information Science Engineering 
+Shri Dharamstal Manjunateshwar College of Engineering and Technology Dharwad 
+Percentage: 8.12 CGPA 
+ 	 
+Pre-University College 
+Prism PU Science College-Dharwad 
+Percentage: 79.83% 
+  
+Schooling 
+Presentation Girls High School-Dharwad 
+Percentage: 83.20% 
+ 
+ 
+PROJECT PROFILE  
+ 
+College Projects  
+Project Title: Campus Tracking System-2017 
+ 
+Description: The objective of this system is to help the visitor explore the campus of College/Office through this application. 
+ 
+Project Title: Bus Tracking System-2018 
+ 
+Description: The objective is to track School/Office buses and obtain their locations. This helps to estimate the arrival time. 
+ 
+SLK POC 
+Project Title: Car Rental System-2019 
+ 
+Description: It is a system design especially for large, premium and small car rental business. The car rental system provides complete functionality of listing and booking car. 
+
+ Project Title: Airline Reservation System-2019 
+ 
+Description: The project Airline Reservation System has been developed on mainframe. In this customers can reserve seat or book flight from any place in the world as long as they are connected to internet. 
+ 
+
+
+TECHNICAL TRAINING UNDERGONE 
+ 
+ 
+1.	Training on OOPS concepts, Database concepts and core Java from 3rd December 2018 to 7th January 2019. 
+2.	Undergone IIHT Training from January 30th 2019 to March 6th 2019 which includes “Java full stack development” at SLK Software service Pvt. Ltd 
+3.	Undergone mainframe Training from May 20th 2019 to June 28th which includes basics of JCL,COBOL,VSAM   
+ 
+
+
+PERSONAL PROFILE 
+  
+Date of Birth  	14th March 1997  
+Mailing Address  	 
+‘Shabari’ Kalmeshwar complex, Srinagar Haliyal Road Dharwad-580003 
+ 
+Languages Known  	English, Kannada, Hindi, Tulu  
+Hobbies 
+ 	Watching movies, Dance, Pencil Sketching 
+ 
+ 
+ 
+ 
